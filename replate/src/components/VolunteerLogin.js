6abb40a74b1527e894d950295b3fc669164c67ed
Brian <lawmen218@yahoo.com>
2019-08-28 21:58:49 -0400

    volunteer sign up working

---
+++ replate/src/components/VolunteerLogin.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { Form, Button } from 'semantic-ui-react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const VolunteerLogin = (props) => {
+    const axiosAuth = () => {
+        axios.post(`https://bw-replate.herokuapp.com/api/auth/volunteer/login`, userCreds)
+            .then(res => {
+                localStorage.setItem('token', res.data.token);
+                props.history.push("/dashboard");
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    const [userCreds, setUserCreds] = useState({
+        username: "",
+        password: ""
+    });
+
+    const login = (userCreds) => {
+        axiosAuth("register", userCreds);
+    }
+    const handleChange = e => {
+        setUserCreds({
+            ...userCreds,
+            [e.target.name]: e.target.value
+        });
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        login(userCreds);
+    }
+    return (
+        <div>
+            <Link to="/dashboard">Dashboard</Link>
+            <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label>Username</label>
+                    <input placeholder="Username" type="text" name="username" value={userCreds.username} onChange={handleChange} />
+                </Form.Field>
+                <Form.Field>
+                    <label>Password</label>
+                    <input placeholder="Password" type="password" name="password" value={userCreds.password} onChange={handleChange} />
+                </Form.Field>
+                <Button type="submit">Log&nbsp;In</Button>
+            </Form>
+        </div>
+    );
+}
+
+export default VolunteerLogin;
