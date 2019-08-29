3b209fe5881f53d25c8edaef4a2a77200efa551c
Brian <lawmen218@yahoo.com>
2019-08-29 00:12:35 -0400

    still working

--- replate/src/components/Sidebar.js
+++ replate/src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { slide as Menu } from 'react-burger-menu';
 import { Button } from 'semantic-ui-react';
@@ -14,6 +14,48 @@ const Sidebar = (props) => {
       <Link to="" className="menu-item">My&nbsp;Account</Link>
     </Menu>
   )
+}
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showMenu: false
+    }
+
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener("click", this.closeMenu);
+    });
+  }
+
+  closeMenu() {
+    this.setState({ showMenu: false }, () => {
+
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onMouseOver={this.showMenu}>Show menu</button>
+        {
+          this.state.showMenu ? (
+            <div className="menu">
+              <button> Business Log In </button>
+              <button> Donar Log In </button>
+            </div>
+          ) : null}
+      </div>
+    );
+  }
 }
 
 export default Sidebar;
